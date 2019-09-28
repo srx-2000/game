084d57d126129366b74a988785205cdfe9a088f8
syz-2018 <huoyanlieniao@163.com>
2019-09-28 23:53:40 +0800

    Gold coin 1
    通过得到鼠标的位置和对比金币text的位置，来使金币增加，如果更改，改坐标就可以了

---
+++ Gold coin/1/金币.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class 金币 : MonoBehaviour
+{
+    public GameObject GO;
+    public GUIText guit;
+    public static int jin = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GO.GetComponent<Text>().text = "20";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Click();
+        }
+     }
+    public string Add(int x)
+    {
+        jin = jin + x;
+        string b = jin.ToString();
+        return b;
+    }
+    public string Reduce(int x)
+    {
+        jin = jin - x;
+        if (!Judge())
+        {
+            jin = 0;
+        }
+        string b = jin.ToString();
+        return b;
+    }
+    public void Click()
+    {
+        float X = Input.mousePosition.x - Screen.width / 2f;
+        float Y = Input.mousePosition.y - Screen.height / 2f;
+        float x = GO.GetComponent<Text>().transform.localPosition.x;
+        float y = GO.GetComponent<Text>().transform.localPosition.y;
+        if (X > (x - 55) && X < (x + 31) && Y > (y+3) && Y < (y + 50))
+        {
+            string c;
+            c = Reduce(5);
+            GO.GetComponent<Text>().text = c;
+        }
+    }
+    public bool Judge()
+    {
+        if (jin > 0)
+        {
+            return true;
+        }
+        else return false;
+    }
+   public int Getnum()
+    {
+        return jin;
+    }
+
+
+}
