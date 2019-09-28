93ee91fd3f723262f84cfcf11de8ec2e7041b30e
syz-2018 <huoyanlieniao@163.com>
2019-09-28 22:16:39 +0800

    Checkpoint
    关卡的原始代码，有增加，减少、自增

---
+++ 关卡原始/关卡.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 关卡 : MonoBehaviour
+{
+    public 金币 jinbi;
+    // Start is called before the first frame update
+    private static int i = 1;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public int GetCheck()
+    {
+        return i;
+    }
+    public void addCheck(int x)
+    {
+        i = i + x;
+        
+    }
+    public void Automatic()
+    {
+        i++;
+    }
+    public void Reduce()
+    {
+        i--;
+    }
+    public void Reduce(int x)
+    {
+        i = i - x;
+    }
+    public bool Judeg()
+    {
+        return jinbi.Judge();
+    }
+
+}
