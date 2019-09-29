ce736adb21be2ed0d77997642b5b3f9c826ce781
srx-2000 <1601684622@qq.com>
2019-09-29 21:04:16 +0800

    progress bar and Music pause bar
    花了好几天的时间去查怎么做血条，中间尝试过用slider最终还是用image的fill完成了，又花了一整天多半天的时间去开始研究unity里面的脚本编写，最后实现了按钮按下进度条增加20%这个十分简单的东西，在研究用slider写血条的时候偶然间看到用slider做调节音频声音大小的东西，顺带手就把我写的网易云音乐的那个加了一个声音条，但是最后的效果只能做到0音量或满音量=
    =具体还得研究

---
+++ Progress bar/bloodtiao.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Globe
+{
+    public static string nextSceneName;
+}
+
+public class bloodtiao : MonoBehaviour
+{
+    public Text loadingText;
+    public Image progressBar;
+    private int curProgressValue = 0;
+    private AsyncOperation operation;
+    public static int a = 0;
+    public static int clicks = 0;
+    // Use this for initialization
+    void onClick()
+    {
+        clicks += 1;
+        Debug.Log("click!" + clicks);
+        a = 1;
+    }
+    void Start()
+    {       //获取按钮游戏对象
+        GameObject btnObj = GameObject.Find("Canvas/Button");
+        //获取按钮脚本组件
+        Button btn = (Button)btnObj.GetComponent<Button>();
+        //添加点击侦听
+        btn.onClick.AddListener(onClick);
+
+        if (SceneManager.GetActiveScene().name == "Loading")
+        {
+            //启动协程
+            StartCoroutine(AsyncLoading());
+        }
+    }
+
+    IEnumerator AsyncLoading()
+    {
+        operation = SceneManager.LoadSceneAsync(Globe.nextSceneName);
+        //阻止当加载完成自动切换
+        operation.allowSceneActivation = false;
+
+        yield return operation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int progressValue = 100;
+
+        if (curProgressValue < progressValue && a == 1)
+        {
+            curProgressValue += 20;
+            a = 0;
+        }
+
+        loadingText.text = curProgressValue + "%";//实时更新进度百分比的文本显示  
+
+        progressBar.fillAmount = curProgressValue / 100f;//实时更新滑动进度图片的fillAmount值  
+
+        if (curProgressValue == 100)
+        {
+            operation.allowSceneActivation = true;//启用自动加载场景  
+            loadingText.text = "OK";//文本显示完成OK  
+
+        }
+    }
+}
---
+++ Progress bar/sliderchange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class sliderchange : MonoBehaviour
+{
+    public Slider s;
+    public int changed=100;
+    public void change()
+    {
+            s.value++;
+           s.value += 0f;
+    }
+}
